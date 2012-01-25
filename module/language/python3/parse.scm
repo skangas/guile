37750c01c2ced4d7d23f4c0a4c54ff6f61ee5c18
Stefan Kangas <skangas@skangas.se>
2012-01-25 16:55:39 +0100

    Add skeleton for Python 3 support

---
+++ module/language/python3/parse.scm
@@ -0,0 +1,28 @@
+;;; Python 3 for Guile
+
+;; Copyright (C) 2012 Stefan Kangas.
+;; Copyright (C) 2012 Per Reimers.
+;; Copyright (C) 2012 David Spångberg.
+;; Copyright (C) 2012 Krister Svanlund.
+
+;;;; This library is free software; you can redistribute it and/or
+;;;; modify it under the terms of the GNU Lesser General Public
+;;;; License as published by the Free Software Foundation; either
+;;;; version 3 of the License, or (at your option) any later version.
+;;;; 
+;;;; This library is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;;; Lesser General Public License for more details.
+;;;; 
+;;;; You should have received a copy of the GNU Lesser General Public
+;;;; License along with this library; if not, write to the Free Software
+;;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+;;; Code:
+
+(define-module (language python3 parse)
+  #:export (read-python3))
+
+(define (read-python3 port)
+  '())
