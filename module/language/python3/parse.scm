5b07f9b870c08eee1ca22a26ee59b96f7bef2c13
David Spångberg <solarus@tunna.org>
2012-02-17 14:26:26 +0100

    python3 parse module now produces an ast

--- module/language/python3/parse.scm
+++ module/language/python3/parse.scm
@@ -22,7 +22,51 @@
 ;;; Code:
 
 (define-module (language python3 parse)
-  #:export (read-python3))
+  #:export (read-python3)
+  #:use-module (srfi srfi-1)
+  #:use-module (ice-9 lineio))
 
+;; Atm this method requires that ast-parser.py is in $PATH. Also since I
+;; couldn't find some way to capture what is written to stdin when
+;; running (system "some-command") this method currently dumps the ast
+;; produced by the ast parser to a temporary file which is later read
+;; into an s-exp.
 (define (read-python3 port)
-  '())
+  (system "rm -f /tmp/python-ast.tmp")
+  (let* ((code (read-string port))
+         (command (string-concatenate `("echo \"" ,code "\" | ast-parser.py > /tmp/python-ast.tmp"))))
+    (system command)
+    (read (open-file "/tmp/python-ast.tmp" "r"))))
+    ;; (read-string (open-file "/tmp/python-ast.tmp" "r"))))
+    ;; ast))
+
+(define (read-string port)
+  (let* ((c (read-char port))
+         (last (list #\newline))
+         (snd-last '())
+         (str last))
+    (while (not (eof-object? c))
+           (set-car! last c)
+           (set-cdr! last (list #\newline))
+           (set! snd-last last)
+           (set! last (cdr last))
+           (set! c (read-char port)))
+    (set-cdr! snd-last '())
+    (list->string str)))
+
+;; ;; non-working hack to save the directory of the current file when
+;; ;; loaded
+;; (eval-when (compile load eval)
+;;   (define load-file-dir '())
+;;   (set! %load-hook (lambda (filename)
+;;                      (set! load-file-dir
+;;                            (let ((pos (string-rindex filename #\/)))
+;;                              (if pos
+;;                                  (substring filename 0 (- pos 1))
+;;                                  filename)))
+;;                      (display "filename = ")
+;;                      (display filename)
+;;                      (newline))))
+
+;; (display load-file-dir)
+;; (newline)
