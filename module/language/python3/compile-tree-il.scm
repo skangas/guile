7da4d63d8f06362e42f00431eb29d585e4a2dfa6
David Spångberg <solarus@tunna.org>
2012-02-21 22:10:41 +0100

    Initial work on the tree-il compilation.

--- module/language/python3/compile-tree-il.scm
+++ module/language/python3/compile-tree-il.scm
@@ -22,8 +22,85 @@
 ;;; Code:
 
 (define-module (language python3 compile-tree-il)
-  #:export (compile-tree-il))
+  #:use-module (language tree-il)
+  #:use-module (system base pmatch)
+  #:use-module (srfi srfi-1)
+  #:export (compile-tree-il test))
+
+;; (define-syntax-rule (-> (type arg ...))
+;;   `(type ,arg ...))
+
+;; (define-syntax-rule (@implv sym)
+;;   (-> (@ '(language ecmascript impl) 'sym)))
+
+;; (define-syntax-rule (@impl sym arg ...)
+;;   (-> (call (@implv sym) arg ...)))
+
+(define (econs name gensym env)
+  ;; (acons name (-> (lexical name gensym)) env))
+  (acons name gensym env))
+
+;; for now only look in local env
+(define (lookup name env)
+  (car (assq-ref env name)))
+
+(define (display-ln obj)
+  (display obj) (newline))
 
 (define (compile-tree-il exp env opts)
-  '())
+  (let ((ret (comp exp '())))
+    (values
+     (parse-tree-il (car ret))
+     env
+     env)))
 
+(define (comp x e)
+  (pmatch x
+    ;; module code
+    ((<mod> ,stmts)
+     (comp-block stmts e))
+     ;; `(begin ,@(map (lambda (stmt) (comp stmt e)) stmts)))
+
+    ;; stmt code
+
+    ;; for now only handle the arguments and no default values
+    ((<fundef> ,id ,args ,body ,decos ,ret)
+     (let* ((argnames (get-args args))
+            (gensyms (map (lambda (x) (gensym (symbol->string x))) argnames)))
+       (list
+        `(define ,id
+           (lambda ()
+             (lambda-case
+              ((,argnames #f #f () () ,gensyms)
+               ,(car (comp-block body (add2env e argnames gensyms)))))))
+        (econs id id e))))
+    ((<return> ,exp)
+     (comp exp e))
+    ((<expr> ,exp)
+     (comp exp e))
+
+    ;; expressions
+    ((<num> ,n)
+     (list `(const ,n) e))
+    ((<name> ,name ,ctx)
+     (list `(lexical name ,(lookup name e)) e))))
+
+(define (comp-block stmts env)
+  (let lp ((in stmts) (out '()) (e env))
+    (pmatch in
+      ((,stmt . ,rest)
+       (let ((ret (comp stmt env)))
+         (lp rest (cons (car ret) out) (cadr ret))))
+      ('()
+       (list `(begin ,@(reverse! out)) env)))))
+
+(define (add2env env args values)
+  (append (zip args values) env))
+
+(define (get-args args)
+  (map (lambda (a) (cadr a)) (cadr args)))
+
+(define (test str)
+  (let ((code ((@ (language python3 parse) read-python3) (open-input-string str))))
+  (display-ln code)
+  (compile-tree-il code '() '())))
